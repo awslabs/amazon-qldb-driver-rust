e5b1a4316bdfb93815ce8750f2c5682e48fb647e
Marc Bowes <marcbowes@gmail.com>
2020-10-08 15:36:50 -0700

    Add the driver
    This commit implements the `QldbDriver` and supporting acts. Here's
    a high-level overview of what we have:
    
    1. QldbDriver can be built with the QldbDriverBuilder
    2. The main API is `transact`, which takes in a function
    3. The function has one argument: a Transaction, with it's own API
    4. Transactions can execute statements or commit/abort
    
    The driver currently only supports an async API, partially because
    that's what Rusuto wants to do under the hood anyways. There is a
    BlockingQldbDriver that simply blocks on any futures. However, this
    driver still exposes the async-Transaction API. As future work, it'd
    be great to have a sync-Transaction API to expose
    easy-to-use (blocking) C bindings.
    
    The transact API has some subtleties in the type signature that are
    worth flagging. First, the param takes a `Fn(Transaction)` as opposed
    to `FnOnce` or `FnMut`. This is because we need to be able to retry
    transactions (making 'once' not suitable) and want to encourage
    idempotent functions (making 'mut' not suitable). Users *can* work
    around idempotency with interior mutability, but that is discouraged.
    
    The next subtly worth calling out is that the Fn must return a special
    type that is hard to construct manually. Instead, users use an API on
    `Transaction` to commit/abort. These methods take the Transaction by
    value, making it hard to incorrectly use the transact method (you need
    to call one of them, and you can't call them twice).
    
    The commit digest is a bit of a dance due to ownership. Initially I
    tried to have the lambda arg be `&mut Transaction` such that executing
    statements could mutate a commit digest in the Transaction. That was a
    very sad path to go down, given current limitations in the type
    system (notably: GAT support). What I settled on is a model where the
    transaction accumulates the digest internally and sends it to the
    driver on Drop (via a channel). Beacuse the disposition methods take
    the tx by value, this has the neat property of immediately making the
    digest available to the driver (which has just await'ed the user fn).
    
    The driver interacts with the pooling and retry functionality to
    acquire sessions and retry transactions. Hopefully it does it
    correctly because there aren't any meaningful tests yet other than the
    example included with this commit, the doctest and my own personal
    testing. The reason there aren't yet tests is I'm still thinking
    through how I want to go about it. (Spoiler: I don't want mocks.)
    
    A couple of other misc. notes:
    
    1. We only support getting values back as Ion binary
    2. Pagination is automatic and blocking (there is no async 'read ahead')
    3. The User-Agent includes Rust but it is prepended not appended [1]
    
    1: https://github.com/rusoto/rusoto/pull/1838

---
+++ examples/main.rs
@@ -0,0 +1,41 @@
+use amazon_qldb_driver::ion_compat;
+use amazon_qldb_driver::QldbDriver;
+use rusoto_core::Region;
+use tokio;
+#[macro_use]
+extern crate log;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    info!("Creating a QLDB driver");
+    let driver = QldbDriver::builder()
+        .ledger_name("sample-ledger")
+        .region(Region::UsWest2)
+        .build()?;
+
+    // Usage example 1: Here we use a closure that returns a `Result<R, QldbError>`. The closure is wrapped in ceremony to appease the type system.
+    info!("Transaction example 1 now running");
+    let results = driver
+        .transact(|mut tx| async {
+            let results = tx
+                .execute_statement("select value 42 from information_schema.user_tables")
+                .await?;
+
+            tx.ok(results).await
+        })
+        .await?;
+    info!("Tx 1 returned {} result(s):", results.len());
+    for reader in results.readers() {
+        let pretty = ion_compat::to_string_pretty(reader?)?;
+        info!("{}", pretty);
+    }
+
+    // No support for Ion yet!
+    // assert_eq!(42, value);
+
+    info!("Goodbye!");
+
+    Ok(())
+}
---
+++ src/transaction.rs
@@ -0,0 +1,140 @@
+use crate::api::{QldbSessionApi, SessionToken, TransactionId};
+use crate::ion_compat::ion_hash;
+use crate::qldb_hash::QldbHash;
+use crate::QldbError;
+use bytes::Bytes;
+use ion_c_sys::reader::IonCReaderHandle;
+use ion_c_sys::result::IonCError;
+use std::convert::TryFrom;
+use std::error::Error as StdError;
+use tokio::sync::mpsc::Sender;
+
+pub struct StatementResults {
+    values: Vec<Bytes>,
+}
+
+impl StatementResults {
+    fn new(values: Vec<Bytes>) -> StatementResults {
+        StatementResults { values }
+    }
+
+    pub fn len(&self) -> usize {
+        self.values.len()
+    }
+
+    pub fn readers(&self) -> impl Iterator<Item = Result<IonCReaderHandle, IonCError>> {
+        self.values
+            .iter()
+            .map(|bytes| IonCReaderHandle::try_from(&bytes[..]))
+    }
+}
+
+pub enum TransactionDisposition {
+    Commit,
+    Abort,
+}
+
+pub struct TransactionAttempt<R> {
+    pub(crate) tx_id: TransactionId,
+    pub(crate) disposition: TransactionDisposition,
+    pub(crate) user_data: R,
+}
+
+pub struct Transaction {
+    client: Box<dyn QldbSessionApi>,
+    session_token: SessionToken,
+    pub tx_id: TransactionId,
+    commit_digest: QldbHash,
+    channel: Sender<QldbHash>,
+}
+
+impl Transaction {
+    pub(crate) fn new(
+        client: Box<dyn QldbSessionApi>,
+        session_token: SessionToken,
+        tx_id: TransactionId,
+        channel: Sender<QldbHash>,
+    ) -> Transaction {
+        let seed_hash = ion_hash(&tx_id);
+        let commit_digest = QldbHash::from_bytes(seed_hash).unwrap();
+        Transaction {
+            client,
+            session_token,
+            tx_id,
+            commit_digest,
+            channel,
+        }
+    }
+
+    // FIXME: params, result, IonHash
+    pub async fn execute_statement<S>(
+        &mut self,
+        statement: S,
+    ) -> Result<StatementResults, QldbError>
+    where
+        S: Into<String>,
+    {
+        let statement = statement.into();
+
+        let first_page = self
+            .client
+            .execute_statement(&self.session_token, &self.tx_id, statement.clone())
+            .await?;
+
+        let statement_hash = QldbHash::from_bytes(ion_hash(&statement)).unwrap();
+        self.commit_digest = self.commit_digest.dot(&statement_hash);
+
+        let mut values = vec![];
+        let mut current = first_page;
+        loop {
+            let page = match &current {
+                Some(_) => current.take().unwrap(),
+                None => break,
+            };
+
+            if let Some(holders) = page.values {
+                for holder in holders {
+                    let bytes = match (holder.ion_text, holder.ion_binary) {
+                        (None, Some(bytes)) => bytes,
+                        (Some(_txt), None) => unimplemented!(), // TextIonCursor::new(txt),
+                        _ => Err(QldbError::UnexpectedResponse(
+                            "expected only one of ion binary or text".to_string(),
+                        ))?,
+                    };
+                    values.push(bytes);
+                }
+
+                if let Some(next_page_token) = page.next_page_token {
+                    let page = self
+                        .client
+                        .fetch_page(&self.session_token, &self.tx_id, next_page_token)
+                        .await?;
+
+                    if let Some(p) = page {
+                        current.replace(p);
+                    }
+                }
+            }
+        }
+
+        Ok(StatementResults::new(values))
+    }
+
+    pub async fn ok<R>(mut self, user_data: R) -> Result<TransactionAttempt<R>, Box<dyn StdError>> {
+        self.channel.send(self.commit_digest).await?;
+
+        Ok(TransactionAttempt {
+            tx_id: self.tx_id,
+            disposition: TransactionDisposition::Commit,
+            user_data: user_data,
+        })
+    }
+
+    pub async fn abort<R>(self, user_data: R) -> Result<TransactionAttempt<R>, Box<dyn StdError>> {
+        Ok(TransactionAttempt {
+            tx_id: self.tx_id,
+            disposition: TransactionDisposition::Abort,
+            user_data: user_data,
+        })
+    }
+}
